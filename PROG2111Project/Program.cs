123a00ebba1893c026389f8587ae4cf859f74f43
Trent <113626643+Platinum-Phantom@users.noreply.github.com>
2025-12-03 20:23:49 -0500

    Working to onnect to the SQL database

--- PROG2111Project/Program.cs
+++ PROG2111Project/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+ * Authours: Josh, Trent
+ * Start: 3/12/2025
+ * End: 
+ * Class: PROG2111
+ * Assignment: PROJECT
+ * Disription: 
+ * Develop C# programs that connect to the MySQL database using the ADO.NET
+• Perform CRUD operations (Create, Read, Update, Delete) for the entities defined
+in your ERD.
+• Ensure proper error handling and transaction control.
+• Use the ADO.NET MySQL library to establish the connection and perform
+operations. 
+ */
+
+using MySql.Data.MySqlClient;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string connStr = "server=localhost;user=testuser;password=Password;database=steamdb;";
+        MySqlConnection conn = new MySqlConnection(connStr);
+        try
+        {
+            conn.Open();
+            Console.WriteLine("Connected!");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Connection Error: " + ex.Message);
+        }
+    }
+}
